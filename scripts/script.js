4e18a35e59f9b0aa3a3b65b7d28a96dfdc98abf0
Damilola Omifare <omifaredammy@gmail.com>
2018-11-05 23:54:21 +0000

    Feat : Add sticky nav
    Description :
    -> modified the js file to add sticky functionality
    -> add sticky class to the css

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,19 @@
-var navBar = $(".nav");
-sticking = "sticky";
-headerAboveNav = $(".header-above-nav").height();
+// When the user scrolls the page, this function executed 
+window.onscroll = function() {afterScrolling()};
 
-$(window).scroll(function() {
-  if ($(this).scrollTop() > headerAboveNav) {
-    navBar.addClass(sticking);
+// Get the navbar
+var navBar = document.getElementById("nav");
+
+// Get the offset position of the navbar
+var sticky = navBar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. 
+// Remove "sticky" when you leave the scroll position
+function afterScrolling() {
+  if (window.pageYOffset >= sticky) {
+    navBar.classList.add("sticky")
   } else {
-    navBar.removeClass(sticking);
+    navBar.classList.remove("sticky");
   }
-});
+};
+
