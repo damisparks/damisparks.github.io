607ba799b2a4fbfc2e96ffcc1c68de09cba77bca
Damilola Omifare <omifaredammy@gmail.com>
2018-11-06 00:09:16 +0000

    Feat : Fixed the sticky navbar [now it is working ]
    Description :
    -> move the sticky navbar script from js file to html under the body element

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,1 @@
-// When the user scrolls the page, this function executed 
-window.onscroll = function() {afterScrolling()};
-
-// Get the navbar
-var navBar = document.getElementById("nav");
-
-// Get the offset position of the navbar
-var sticky = navBar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. 
-// Remove "sticky" when you leave the scroll position
-function afterScrolling() {
-  if (window.pageYOffset >= sticky) {
-    navBar.classList.add("sticky")
-  } else {
-    navBar.classList.remove("sticky");
-  }
-};
 
